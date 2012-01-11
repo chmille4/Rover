3bbe6b694c6a023362506ca6a9cfc4bccf75c19b
Chase Miller <chmille4@gmail.com>
2012-01-11 15:44:10 -0500

    Fix behavior where drawStyle gets lost when semantic zoom out changes to line drawStyle

--- js/rover.track.js
+++ js/rover.track.js
@@ -498,11 +498,13 @@ var RoverTrack = Class.extend({
    setupDefaultScribl: function(chart) {
       chart.offset = 0;
       chart.trackHooks.push( function(track) { 
-         if (track.chart.ntsToPixels() > 70)
-            track.drawStyle = 'line';
-         else
-            track.drawStyle = 'expand';
-         // return false to allow normal draing of glyph
+         if (track.chart.ntsToPixels() > 70) {
+            track.chart.previousDrawStyle = track.getDrawStyle();
+            track.chart.drawStyle = 'line';            
+         } else if (track.previousDrawStyle) {
+            track.chart.drawStyle = track.chart.previousDrawStyle;
+            track.chart.previousDrawStyle = undefined;
+         }
          return false;
       });
       chart.laneSizes = this.laneSizes;
