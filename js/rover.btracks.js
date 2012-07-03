a694bbe711e6fb4f02ffa0888cbe33f4cbb94547
Chase Miller <chmille4@gmail.com>
2012-07-03 10:28:44 -0400

    Make zoom plus and minus clickable

--- js/rover.btracks.js
+++ js/rover.btracks.js
@@ -1018,39 +1018,29 @@ window.ZoomView = Backbone.View.extend({
             // rover.zoom(numNtsToShow);
           },
          change: function(event,ui) {
+            if (!event.originalEvent) {
+               $('#zoom-slider').slider('option', 'start').call($('#zoom-slider'));
+               $('#zoom-slider').slider('option', 'slide').call($('#zoom-slider'), event, ui);
+            }
+             var bufferSize = rover.getBufferWidth();
+             var newMin = Math.max( rover.get('displayMin') - bufferSize, 1 );
+             var newMax = rover.get('displayMax') + bufferSize;
 
-            // check if event was fired by user and if so, proceed
-            // this stops change from firing when programmatically changing value
-            if (event.originalEvent) {
-                //var bufferSize = (rover.scale.scale.max - rover.scale.scale.min) * rover.bufferMultiple;
-                // var max = rover.get('max');
-                // var min = rover.get('min');
-                var bufferSize = rover.getBufferWidth();
-                var newMin = Math.max( rover.get('displayMin') - bufferSize, 1 );
-                var newMax = rover.get('displayMax') + bufferSize;
-
-                // var totalNts = newMax - newMin;
-                // var widthNts = max - min;
-                // var widthPx = $(rover.canvasContentDiv).width();
-                // var totalPx = widthPx / (widthNts / totalNts);
-                // var leftNts = min;
-                
-                if ( newMin < rover.oldMin || newMax > rover.oldMax ) {
-                   rover.set( {min:newMin, max:newMax} );
-                   _(rover.tracks.models).each( function(track) {
-                      track.center.chart.fetch({
-                         data: $.param({min:newMin, max:newMax}),
-                         error: function(){alert('error fetching');}
-                      });     
-                      track.trigger('fetching');                 
-                   });
-                } else {
-                   rover.set({ min:newMin, max:newMax });
-                }
-                
-                
+             if ( newMin < rover.oldMin || newMax > rover.oldMax ) {
+                rover.set( {min:newMin, max:newMax} );
+                _(rover.tracks.models).each( function(track) {
+                   track.center.chart.fetch({
+                      data: $.param({min:newMin, max:newMax}),
+                      error: function(){alert('error fetching');}
+                   });     
+                   track.trigger('fetching');                 
+                });
+             } else {
+                rover.set({ min:newMin, max:newMax });
              }
-         }
+       
+       
+          }
       });      
       
       //this.template = Handlebars.compile( $('#zoom-template').html() );
